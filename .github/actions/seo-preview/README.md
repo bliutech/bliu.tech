344bfb1d17efb4aa2f4c020a326484644498cb04
Benson Liu <bensonhliu@gmail.com>
2024-12-26 02:35:23 -0500

    chore(ci): add `workflows_dispatch` event to actions

---
+++ .github/actions/seo-preview/README.md
@@ -0,0 +1,2 @@
+# SEO Preview
+Generates an image that can be used as a preview image for a website (in either the `og:image` or `twitter:image` meta tags).
---
+++ .github/actions/seo-preview/preview.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Set the viewport's width and height
+  await page.setViewport({ width: 1366, height: 768 });
+
+  // Open ScrapingBee's home page
+  await page.goto('http://localhost:3000');
+
+  try {
+    // Capture screenshot and save it in the current folder:
+    await page.screenshot({ path: `./bliutech_homepage.jpg` });
+
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  } finally {
+    await browser.close();
+    console.log(`Screenshot has been captured successfully`);
+  }
+})();
+
